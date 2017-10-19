2ee937cd38bcaec2041cfb0e20cdb09306c49d3f
lumpydumpy <adamreifsneider@gmail.com>
2017-10-18 20:34:37 -0400

    remove label for non-pr

--- src/pull_automerge/core.clj
+++ src/pull_automerge/core.clj
@@ -28,35 +28,33 @@
       "&page=0&per_page=1"
       "&sort=created&direction=asc"))
 
-(defn generate-merge-url [pr-number]
+(defn generate-delete-label-url [org repo issue-number label]
   (str 
-  "https://api.github.com/repos/AdamReifsneider/api-testing/pulls/" 
-    pr-number 
-    "/merge"))
+  "https://api.github.com/repos/" org "/" repo "/issues/" issue-number "/labels/" label))
 
-(defn merge-pull-number [pull-number options]
-  (println "Merging PR#" pull-number)
-    (def merge-result @(http/put (generate-merge-url pull-number) options))
-    (println "Merge status: " (merge-result :status))
-    (if (merge-result :error)
-      (println "MERGE ERROR: " (merge-result :error))))
+; (defn merge-pull-number [pull-number options]
+;   (println "Merging PR#" pull-number)
+;     (def merge-result @(http/put (generate-merge-url pull-number) options))
+;     (println "Merge status: " (merge-result :status))
+;     (if (merge-result :error)
+;       (println "MERGE ERROR: " (merge-result :error))))
 
-(defn try-merge [pull-url options]
-  (def pull-result @(http/get pull-url options))
-    (println "Retrieve pull status: " (pull-result :status))
-    (def pull (parse-string (pull-result :body) true))
-    (if (and (= (pull :mergeable) true) (= (pull :mergeable_state) "clean"))
-      (merge-pull-number (pull :number) options)
-      (println (str "Cannot merge oldest PR#" (pull :number) ":")
-        "\n  mergeable =" (pull :mergeable)
-        "\n  mergeable_state =" (pull :mergeable_state))))
+; (defn try-merge [pull-url options]
+;   (def pull-result @(http/get pull-url options))
+;     (println "Retrieve pull status: " (pull-result :status))
+;     (def pull (parse-string (pull-result :body) true))
+;     (if (and (= (pull :mergeable) true) (= (pull :mergeable_state) "clean"))
+;       (merge-pull-number (pull :number) options)
+;       (println (str "Cannot merge oldest PR#" (pull :number) ":")
+;         "\n  mergeable =" (pull :mergeable)
+;         "\n  mergeable_state =" (pull :mergeable_state))))
 
 (defn -main
   [& args]
   (def token (first args))
   (def org "ai-labs-team")
   (def repo "axiom-platform")
-  (def label "DevOps")
+  (def label "Automerge")
   (def options (generate-options token))
   ; TODO crawl to /issues from root url
   (def pulls-result @(http/get 
@@ -70,5 +68,14 @@
         org "/" repo "' with label '" label "'"))
       (println "Exiting...")
       (System/exit 0)))
-  (println "Found PR: " ((first pulls) :title))
+  (def pull (first pulls))
+  (def pull-number (pull :number))
+  (println "Found Issue with Title/Number: " (pull :title) "/" pull-number)
+  (if (nil? (pull :pull_request))
+    (do
+      (println "No key 'pull_request' present. Removing label" label)
+      (def delete-url (generate-delete-label-url org repo pull-number label))
+      (def delete-result @(http/delete delete-url options))
+      (println "Delete label result:" (delete-result :status)))
+  )
 )
