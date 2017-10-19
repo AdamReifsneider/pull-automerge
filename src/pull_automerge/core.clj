2a7b1d0c9f07d8da988b426b305f129532d70f6b
lumpydumpy <adamreifsneider@gmail.com>
2017-10-18 22:26:33 -0400

    dirty state

--- src/pull_automerge/core.clj
+++ src/pull_automerge/core.clj
@@ -32,6 +32,20 @@
   (str 
   "https://api.github.com/repos/" org "/" repo "/issues/" issue-number "/labels/" label))
 
+(defn remove-label-and-exit [options org repo issue-number label reason]
+  (println reason)
+  (println (str "Removing label '" label "'"))
+  (def delete-url (generate-delete-label-url org repo issue-number label))
+  (def delete-result @(http/delete delete-url options))
+  (println "Delete label result:" (delete-result :status))
+  (System/exit 0))
+
+(defn get-pull-request [options org repo pull-number]
+  (def result @(http/get 
+    (str "https://api.github.com/repos/" org "/" repo "/pulls/" pull-number) options))
+  (println (str "Get pull request " pull-number " status: " (result :status)))
+  (parse-string (result :body) true))
+
 ; (defn merge-pull-number [pull-number options]
 ;   (println "Merging PR#" pull-number)
 ;     (def merge-result @(http/put (generate-merge-url pull-number) options))
@@ -62,20 +76,60 @@
     options))
   (println "Retrieve automerge pulls status: " (pulls-result :status))
   (def pulls(parse-string (pulls-result :body) true))
+
   (if (= 0 (count pulls))
     (do    
       (println (str "No automergeable pull requests in '" 
         org "/" repo "' with label '" label "'"))
       (println "Exiting...")
       (System/exit 0)))
+
   (def pull (first pulls))
   (def pull-number (pull :number))
   (println "Found Issue with Title/Number: " (pull :title) "/" pull-number)
+
+
   (if (nil? (pull :pull_request))
-    (do
-      (println "No key 'pull_request' present. Removing label" label)
-      (def delete-url (generate-delete-label-url org repo pull-number label))
-      (def delete-result @(http/delete delete-url options))
-      (println "Delete label result:" (delete-result :status)))
-  )
+    (do (remove-label-and-exit options org repo pull-number label
+      "Issue has no key 'pull_request' present, so it must not be a pull request.")))
+
+  (def pull (get-pull-request options org repo 1242))
+  (def state (pull :mergeable_state))
+  ; (print-json-object pull)
+
+  (if (= "dirty" state)
+    (do (remove-label-and-exit options org repo pull-number label
+      "Issue mergeable_state is dirty.")))
+  ; exit
+  ; (def statuses-result @(http/get (pull :statuses_url) options))
+  ; (println (statuses-result :status))
+  ; (print-json-string (statuses-result :body))
 )
+
+;;mergeable_state dirty means merge conflict?
+;;mergeable_state blocked means requests changes?
+;;mergeable_state behind means out of date
+;;mergeable_state clean means it can be merged?
+;;mergeable means the github can do an auto merge
+
+;;to update a PR: https://developer.github.com/v3/pulls/#update-a-pull-request
+
+;;behind takes priority over blocked -- maybe check out statuses EEEEEWWWWWW
+
+;;1142: merge conflicts, approved
+;; mergeable_state: dirty
+;; mergeable: false
+
+;;1247 out of date, no merge conflicts, needs review, tests pass
+;; mergeable_state: behind
+;; mergeable: true (i have admin, though...)
+
+;;1178 up to date, approved, tests pass
+;; mergeable_state: clean
+;; mergeable: true
+
+;;8 up to date, no merge conflicts, requested changes
+;;mergeable_state: blocked
+;;mergeable: true
+
+;;1189 out of date, no merge conflicts, requested changes
