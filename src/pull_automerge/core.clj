c439e8cde721ffd1c6ae90368232d29b454ebe88
CliffDavis <cliffd@advisoril.com>
2018-01-24 10:54:30 -0500

    verify 1,2,3,4,5...five statuses. ah hah hah

--- src/pull_automerge/core.clj
+++ src/pull_automerge/core.clj
@@ -129,16 +129,20 @@
         (do (println "Must wait for " required-status " result.") false)
         (not (= "success" state))))))
 
-(defn handle-blocked-state [options org repo label pull state required-status-1 required-status-2]
-; (defn handle-blocked-state [options org repo label pull state required-status-1]
+(defn handle-blocked-state [options org repo label pull state required-status-1 required-status-2 required-status-3 required-status-4 required-status-5]
   (def head-sha ((pull :head) :sha))
   (def statuses (parse-string ((statuses-for-ref options org repo head-sha) :body)))
   (def failed-1 (required-status-failed head-sha statuses required-status-1))
   (def failed-2 (required-status-failed head-sha statuses required-status-2))
+  (def failed-3 (required-status-failed head-sha statuses required-status-3))
+  (def failed-4 (required-status-failed head-sha statuses required-status-4))
+  (def failed-5 (required-status-failed head-sha statuses required-status-5))
   (println (str required-status-1 " failed: " failed-1))
   (println (str required-status-2 " failed: " failed-2))
-  (if (or failed-1 failed-2)
-  ; (if failed-1
+  (println (str required-status-3 " failed: " failed-3))
+  (println (str required-status-4 " failed: " failed-4))
+  (println (str required-status-5 " failed: " failed-5))
+  (if (or failed-1 failed-2 failed-3 failed-4 failed-5)
     (remove-label options org repo pull-number label
       (str "Pull request's 'mergeable_state is' '" state "':"
         " lacks approval or has requested changes"))))
