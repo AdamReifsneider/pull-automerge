5f267a7f6c6e19556d34653cdc437f7bf49fa630
CliffDavis <cliffd@advisoril.com>
2018-09-26 14:08:35 -0400

    update to work with new workflow and with java 10

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject pull-automerge "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :jvm-opts ["--add-modules" "java.xml.bind"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"] 
+    [org.clojure/clojure "1.8.0"]
     [org.clojure/data.json "0.2.6"]
     [http-kit "2.2.0"]
     [cheshire "5.8.0"]
